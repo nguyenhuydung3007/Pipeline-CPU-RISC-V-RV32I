564dad4a71637d1f734caa0f5db0c815d84efeb3
nguyenhuydung3007 <nguyenhuydung3072005@gmail.com>
2026-04-28 08:15:12 +0700

    Update Data Bus (Thêm VGA)

--- CORE/Core/Data_Bus.v
+++ CORE/Core/Data_Bus.v
@@ -1,3 +1,15 @@
+// ==========================================================
+// Module Data_Bus
+// CPU System Bus
+//
+// Memory Map
+// ----------------------------------------------------------
+// 0x0000_0000 ~ 0x0FFF_FFFF -- RAM
+// 0x1000_0000 ~ 0x1000_00FF -- GPIO
+// 0x2000_0000 ~ 0x2000_00FF -- UART
+// 0x3000_0000 ~ 0x3000_2FFF -- VGA
+// ==========================================================
+
 module Data_Bus (
 
     input [31:0] addr,
@@ -5,22 +17,27 @@ module Data_Bus (
     input        write_en,
     input        read_en,
 
-    // RAM
+    // =============== RAM ===============
     input [31:0] ram_rd_data,
     output       ram_we,
     output       ram_re,
 
-    // GPIO
+    // =============== GPIO ===============
     input [31:0] gpio_rd_data,
     output       gpio_we,
     output       gpio_re,
 
-    // UART
+    // =============== UART ===============
     input [31:0] uart_rd_data,
     output       uart_we,
     output       uart_re,
 
-    // CPU Read data
+    // =============== VGA ===============
+    input [31:0] vga_rd_data,
+    output       vga_we,
+    output       vga_re,
+
+    // =============== CPU READ ===============
     output reg [31:0] rd_data
 );
 
@@ -29,19 +46,29 @@ module Data_Bus (
     // ===============================================
 
     // =============== GPIO MEMORY ===============
-    parameter GPIO_ADDR         = 32'h1000_0000;
-    parameter GPIO_SIRE_ADDR    = 32'h0000_0100;
+    localparam GPIO_ADDR    = 32'h1000_0000;
+    localparam GPIO_SIRE    = 32'h0000_0100;
 
     // =============== UART MEMORY ===============
-    parameter UART_ADDR         = 32'h2000_0000;
-    parameter UART_SIZE_ADDR    = 32'h0000_0100;
+    localparam UART_ADDR    = 32'h2000_0000;
+    localparam UART_SIZE    = 32'h0000_0100;
+
+    // =============== VGA MEMORY ===============
+    localparam VGA_ADDR     = 32'h3000_0000;
+    localparam VGA_SIZE     = 32'h3000_3000;    // 12KB
 
     // ===============================
     // Address Mapp
     // ===============================
-    wire sel_ram    = (addr < GPIO_ADDR);
-    wire sel_gpio   = (addr >= GPIO_ADDR && addr < (GPIO_ADDR + GPIO_SIRE_ADDR));
-    wire sel_uart   = (addr >= UART_ADDR && addr < (UART_ADDR + UART_SIZE_ADDR));
+    wire sel_ram;
+    wire sel_gpio;
+    wire sel_uart;
+    wire sel_vga;
+
+    assign sel_ram    = (addr < GPIO_ADDR);
+    assign sel_gpio   = (addr >= GPIO_ADDR && addr < (GPIO_ADDR + GPIO_SIRE));
+    assign sel_uart   = (addr >= UART_ADDR && addr < (UART_ADDR + UART_SIZE));
+    assign sel_vga    = (addr >= VGA_ADDR  && addr < (VGA_ADDR  + VGA_SIZE));
 
     // ==============================
     // Write Enable
@@ -49,13 +76,15 @@ module Data_Bus (
     assign ram_we   = write_en && sel_ram;
     assign gpio_we  = write_en && sel_gpio;
     assign uart_we  = write_en && sel_uart;
+    assign vga_we   = write_en && sel_vga;
 
     // =============================
     // Read Enable
     // =============================
     assign ram_re   = read_en && sel_ram;
     assign gpio_re  = read_en && sel_gpio;
-    assign uart_re  = read_en  && sel_uart;
+    assign uart_re  = read_en && sel_uart;
+    assign vga_re   = read_en && sel_vga;
 
     // ============================
     // Read Data MUX
@@ -63,19 +92,23 @@ module Data_Bus (
     always @(*) begin
         
         if (sel_ram) begin
-             rd_data    = ram_rd_data;
+             rd_data = ram_rd_data;
         end
 
         else if (sel_gpio) begin
-            rd_data     = gpio_rd_data;
+            rd_data = gpio_rd_data;
         end
 
         else if (sel_uart) begin
-            rd_data     = uart_rd_data;
+            rd_data = uart_rd_data;
+        end
+
+        else if (sel_vga) begin
+            rd_data = vga_rd_data;
         end
 
         else begin
-            rd_data     = 32'd0;
+            rd_data = 32'd0;
         end
 
     end 
